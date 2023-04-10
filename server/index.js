4f0523e7d1d9b64c1d3732c9b7e0b05a5a036ec0
Joshua <cutejosh2@gmail.com>
2023-04-11 00:52:21 +0100

    basic login feat

--- server/index.js
+++ server/index.js
@@ -9,7 +9,8 @@ import morgan from 'morgan'
 import path from 'path'
 import { fileURLToPath } from "url"
 
-import {register} from "./controllers/auth.js"
+import authRoutes from "./routes/auth.js"
+import { register } from "./controllers/auth.js"
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url)
@@ -38,6 +39,9 @@ const upload = multer({ storage: storage })
 
 // ROUTES WITH FILES
 app.post("/auth/register", upload.single("picture"), register)
+
+// ROUTES
+app.use("/auth", authRoutes)
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001
