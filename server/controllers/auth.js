b4bb1708f97a81ddd02f0a4ccfaa17d829e50d48
Joshua <cutejosh2@gmail.com>
2023-04-11 00:32:30 +0100

    register feat

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,32 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
+
+
+// register user
+export const register = async (req, res) => {
+	try {
+		const { firstName, lastName, email, password, picturePath, friends, location, occupation } = req.body;
+		
+		const salt = await bcrypt.genSalt();
+		const hashedPassword = await bcrypt.hash(password, salt);
+
+		const newUser = new User({
+			firstName,
+			lastName,
+			email,
+			password: hashedPassword,
+			picturePath,
+			friends,
+			location,
+			occupation,
+			viewedProfile: Math.floor(Math.random() * 1000),
+			impressions: Math.floor(Math.random() * 1000),
+		})
+		const savedUser = await newUser.save();
+		res.status(201).json(savedUser);
+
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+}
