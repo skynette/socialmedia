5446a153d6636e6ba4c95d991471635253357296
Joshua <cutejosh2@gmail.com>
2023-04-11 01:19:14 +0100

    created verifyToken middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken';
+
+export const verifyToken = async (req, res, next) => {
+	try {
+		let token = req.header("Authorization");
+
+		if (!token) return res.status(403).json({ message: "Token not found" });
+
+		if (token.startsWith("Bearer ")) {
+			token = token.slice(7, token.length).trimLeft();
+		}
+
+		const verified = jwt.verify(token, process.env.JWT_SECRET);
+		if (!verified) return res.status(401).json({ message: "Token verification failed" });
+		req.user = verified;
+		next();
+
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+}
