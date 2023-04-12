fb5c5ea2cd108acbf366c8e253d69ce4507428c3
Joshua <cutejosh2@gmail.com>
2023-04-12 09:15:26 +0100

    added dummy data

--- server/index.js
+++ server/index.js
@@ -17,6 +17,10 @@ import { createPost } from "./controllers/posts.js"
 
 import { verifyToken } from "./middleware/auth.js"
 
+import User from "./models/User.js"
+import Post from "./models/Post.js"
+import { users, posts } from "./data/index.js"
+
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -57,5 +61,7 @@ mongoose.connect(process.env.MONGOOSE_URL, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 }).then(() => {
-	app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+	app.listen(PORT, () => console.log(`Server started and listening on Port: ${PORT}`))
+	User.insertMany(users)
+	Post.insertMany(posts)
 }).catch((error) => console.log(`${error} did not connect`))
