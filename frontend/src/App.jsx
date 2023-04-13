deca9fa2f167b86e93060d92e92647121fdbbfa1
Joshua <cutejosh2@gmail.com>
2023-04-13 16:49:13 +0100

    setup store

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,26 @@ import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom"
 import HomePage from "./scenes/homePage"
 import LoginPage from "./scenes/loginPage"
 import ProfilePage from "./scenes/profilePage"
+import { Provider } from "react-redux"
+import store from "./store"
+import { PersistGate } from "redux-persist/lib/integration/react"
+import { persistStore } from "redux-persist"
+
 
 
 const App = () => {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route exact path="/" element={<LoginPage />} />
-				<Route exact path="/home" element={<HomePage />} />
-				<Route exact path="/profile/:userId" element={<ProfilePage />} />
-			</Routes>
-		</BrowserRouter>
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistStore(store)}>
+				<BrowserRouter>
+					<Routes>
+						<Route exact path="/" element={<LoginPage />} />
+						<Route exact path="/home" element={<HomePage />} />
+						<Route exact path="/profile/:userId" element={<ProfilePage />} />
+					</Routes>
+				</BrowserRouter>
+			</PersistGate>
+		</Provider>
 	)
 }
 
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authSlice from 'state/authSlice';
+import authReducer from './state/authSlice';
+import { persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist"
+import storage from "redux-persist/lib/storage"
+
+const persistConfig = { key: "root", storage, version: 1 }
+const persistedReducer = persistReducer(persistConfig, authReducer)
 
 export const store = configureStore({
-	reducer: {
-	},
+	reducer: persistedReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
 })
+
+export default store
