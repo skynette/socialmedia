3d49e48432814baaf1070c5bdf83ab180d1aacd4
Joshua <cutejosh2@gmail.com>
2023-04-13 22:34:51 +0100

    setting up form

---
+++ frontend/src/scenes/loginPage/Form.jsx
@@ -0,0 +1,21 @@
+import { Box, Button, TextField, useMediaQuery, Typography, useTheme } from '@mui/material'
+import { useState } from 'react'
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined'
+import { Formik } from 'formik'
+import * as yup from 'yup'
+import { useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { setLogin } from 'state/authSlice'
+import Dropzone from 'react-dropzone'
+import FlexBetween from '@components/FlexBetween'
+
+
+const Form = () => {
+  return (
+	<Box>
+
+	</Box>
+  )
+}
+
+export default Form
--- frontend/src/scenes/loginPage/index.jsx
+++ frontend/src/scenes/loginPage/index.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { Box, Typography, useTheme, useMediaQuery } from '@mui/material'
+import Form from './form'
 
-const index = () => {
-  return (
-	<div>index</div>
-  )
+const LoginPage = () => {
+	const theme = useTheme()
+	const isNonMobileScreens = useMediaQuery(theme.breakpoints.up('md'))
+	const primaryLight = theme.palette.primary.light
+
+	return (
+		<Box>
+			<Box width="100%" backgroundColor={theme.palette.background.alt}
+				p="1rem 6%"
+				textAlign="center"
+			>
+				<Typography
+					fontWeight="bold"
+					fontSize="32px"
+					color="primary"
+					sx={{
+						"&:hover": {
+							color: primaryLight,
+							cursor: "pointer",
+						}
+					}}
+				>
+					SocialMedia
+				</Typography>
+			</Box>
+			<Box width={isNonMobileScreens ? "50px" : "93%"}
+			p="2rem"
+			m="2rem auto"
+			borderRadius="1.5rem"
+			backgroundColor={theme.palette.background.alt}
+			>
+				<Typography fontWeight="500" variant="h5" sx={{mb: "1.5rem"}}>
+					Welcome to SocialMedia, please login to continue
+				</Typography>
+				<Form />
+			</Box>
+		</Box>
+	)
 }
 
-export default index
+export default LoginPage
