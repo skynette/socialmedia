93ba095dbf900c1d181396807cceb6b3285f8a17
Joshua <cutejosh2@gmail.com>
2023-04-13 20:55:02 +0100

    default color scheme

--- frontend/src/scenes/navbar/index.jsx
+++ frontend/src/scenes/navbar/index.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
 	const neutralLight = theme.palette.neutral.light
 	const dark = theme.palette.neutral.dark
 	const background = theme.palette.background.default
-	const primaryLight = theme.palette.primary.light
+	const primaryLight = theme.palette.primary.dark
 	const alt = theme.palette.background.alt
 
 	const fullName = `${user.firstName} ${user.lastName}`
