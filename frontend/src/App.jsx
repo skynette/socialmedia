a40351aeb05a843f5960d0d2788ea11d455a1aba
Joshua <cutejosh2@gmail.com>
2023-04-13 17:12:06 +0100

    setup theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,26 +2,33 @@ import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom"
 import HomePage from "./scenes/homePage"
 import LoginPage from "./scenes/loginPage"
 import ProfilePage from "./scenes/profilePage"
-import { Provider } from "react-redux"
 import store from "./store"
 import { PersistGate } from "redux-persist/lib/integration/react"
 import { persistStore } from "redux-persist"
 
-
+import { useMemo } from "react"
+import { useSelector } from "react-redux"
+import { CssBaseline, ThemeProvider } from "@mui/material"
+import { createTheme } from "@mui/material"
+import { themeSettings } from "./theme"
 
 const App = () => {
+	const mode = useSelector((store) => store.mode)
+	const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+	
 	return (
-		<Provider store={store}>
-			<PersistGate loading={null} persistor={persistStore(store)}>
-				<BrowserRouter>
+		<PersistGate loading={null} persistor={persistStore(store)}>
+			<BrowserRouter>
+				<ThemeProvider theme={theme}>
+					<CssBaseline />
 					<Routes>
 						<Route exact path="/" element={<LoginPage />} />
 						<Route exact path="/home" element={<HomePage />} />
 						<Route exact path="/profile/:userId" element={<ProfilePage />} />
 					</Routes>
-				</BrowserRouter>
-			</PersistGate>
-		</Provider>
+				</ThemeProvider>
+			</BrowserRouter>
+		</PersistGate>
 	)
 }
 
