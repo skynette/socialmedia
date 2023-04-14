ccdd23acd9950958115f9f23479dffcbeaba6b29
Joshua <cutejosh2@gmail.com>
2023-04-14 22:22:24 +0100

    login area

--- frontend/src/scenes/loginPage/Form.jsx
+++ frontend/src/scenes/loginPage/Form.jsx
@@ -74,9 +74,9 @@ const Form = () => {
 						gridTemplateColumns="repeat(4, minmax(0, 1fr))"
 						sx={{
 							"& > div": { gridColumn: isNonMobileScreens ? undefined : "span 4" },
-						  }}
+						}}
 					>
-						{isLogin && (
+						{isRegister && (
 							<>
 								<TextField
 									label="First Name"
@@ -155,12 +155,71 @@ const Form = () => {
 							</>
 						)}
 
+						{/* LOGIN */}
+
+						<TextField
+							label="Email"
+							onBlur={handleBlur}
+							onChange={handleChange}
+							value={values.email}
+							name='email'
+							error={Boolean(touched.email) && Boolean(errors.email)}
+							helperText={touched.email && errors.email}
+							sx={{ gridColumn: "span 4" }}
+						/>
+						<TextField
+							label="Password"
+							type="password"
+							onBlur={handleBlur}
+							onChange={handleChange}
+							value={values.password}
+							name='password'
+							error={Boolean(touched.password) && Boolean(errors.password)}
+							helperText={touched.password && errors.password}
+							sx={{ gridColumn: "span 4" }}
+						/>
+
+						{/* BUTTONS */}
+						<Box>
+							<Button
+								fullWidth
+								type='submit'
+								sx={{
+									m: "2rem 0",
+									p: "1rem",
+									backgroundColor: palette.primary.main,
+									color: palette.background.alt,
+									"&:hover": { color: palette.primary.main }
+								}}
+							>
+								{isLogin ? "Login" : "Register"}
+
+							</Button>
+							<Typography
+								onClick={() => {
+									setPageType(isLogin ? "register" : "login");
+									resetForm();
+								}}
+								sx={{
+									textDecoration: "underline",
+									color: palette.primary.main,
+									"&:hover": {
+										color: palette.primary.light,
+										cursor: "pointer",
+									}
+								}}
+							>
+								{isLogin ? "Don't have an account? Sign up" : "Already have an account? Login"}
+							</Typography>
+						</Box>
+
 					</Box>
 
 				</form>
-			)}
+			)
+			}
 
-		</Formik>
+		</Formik >
 	)
 }
 
