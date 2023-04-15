079c8699277545584d5e865cb3b7ca4ca07e4f03
Joshua <cutejosh2@gmail.com>
2023-04-15 01:38:06 +0100

    user widget done

--- frontend/src/scenes/widgets/UserWidget.jsx
+++ frontend/src/scenes/widgets/UserWidget.jsx
@@ -92,7 +92,49 @@ const UserWidget = ({ userId, picturePath }) => {
 				{/* third row */}
 				<Box p="1rem 0">
 					<FlexBetween mb="0.5rem">
+						<Typography color={medium}>Who's viewed your profile</Typography>
+						<Typography color={medium} fontWeight="500">{viewedProfile}</Typography>
+					</FlexBetween>
+					<FlexBetween>
+						<Typography color={medium}>Impressions of your post</Typography>
+						<Typography color={medium} fontWeight="500">{impressions}</Typography>
+					</FlexBetween>
+				</Box>
 
+				{/* fourth row */}
+				<Box>
+					<Typography fontSize="1rem" color={main} fontWeight="500" mb="1rem">
+						Social Profiles
+					</Typography>
+
+					<FlexBetween gap="1rem" mb="0.5rem">
+						<FlexBetween gap="1rem">
+							<img src="../assets/twitter.png" alt="twitter" />
+							<Box>
+								<Typography color={main} fontWeight="500">
+									Twitter
+								</Typography>
+								<Typography color={medium}>
+									Social Network
+								</Typography>
+							</Box>
+						</FlexBetween>
+						<EditOutlined sx={{color: main}}/>
+					</FlexBetween>
+
+					<FlexBetween gap="1rem">
+						<FlexBetween gap="1rem">
+							<img src="../assets/linkendin.png" alt="linkendin" />
+							<Box>
+								<Typography color={main} fontWeight="500">
+									LinkendIn
+								</Typography>
+								<Typography color={medium}>
+									Network Platform
+								</Typography>
+							</Box>
+						</FlexBetween>
+						<EditOutlined sx={{color: main}}/>
 					</FlexBetween>
 				</Box>
 
